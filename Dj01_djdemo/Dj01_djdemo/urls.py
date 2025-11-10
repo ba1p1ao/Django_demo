4cd7ca28b6c0c8ebbcd87538650f8534a5ac370e
ba1p1ao <ba1p1ao>
2025-11-10 13:53:02 +0800

    添加: 路由

--- Dj01_djdemo/Dj01_djdemo/urls.py
+++ Dj01_djdemo/Dj01_djdemo/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from goods import views as goodsViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("index/", goodsViews.index)
 ]
