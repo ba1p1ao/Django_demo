93f7c3d8056609b68f80fc94b0728d99789b45c6
ba1p1ao <1732734010@qq.com>
2025-12-02 16:30:08 +0800

    添加: menu类视图文件

--- muxishop/apps/menu/views.py
+++ muxishop/apps/menu/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpRequest, HttpResponse, JsonResponse
+from apps.menu import models
+from utils.ResponseMessage import MenuResponse
 
-# Create your views here.
+class GoodsMainMenuView(View):
+    def get(self, request: HttpRequest):
+        menu_all = models.MainMenu.objects.all()
+
+        print(menu_all)
+        menu_list = []
+        for m in menu_all:
+            menu_list.append(m.__str__())
+
+        # response_data = {
+        #     "status": 200,
+        #     "data": menu_list
+        # }
+        return MenuResponse.success(menu_list, safe=False)
+    
+
+
+class GoodsSubMenuView(View):
+    def get(self, request: HttpRequest):
+        main_menu_id = request.GET.get("main_menu_id")
+        
+        sub_menu = models.SubMenu.objects.filter(main_menu_id=main_menu_id)
+
+        sub_menu_list = []
+        for sm in sub_menu:
+            sub_menu_list.append(sm.__str__())
+        # response_data = {
+        #     "status": 200,
+        #     "data": sub_menu.__str__()
+        # }
+        return MenuResponse.success(sub_menu_list, safe=False)
