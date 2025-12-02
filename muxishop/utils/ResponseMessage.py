b214d2d02473dc2507b60867cfa265037a16ebf5
ba1p1ao <1732734010@qq.com>
2025-12-02 18:11:38 +0800

    添加: goods相应数据

--- muxishop/utils/ResponseMessage.py
+++ muxishop/utils/ResponseMessage.py
@@ -7,6 +7,7 @@
 from django.http import JsonResponse
 
 
+# 菜单响应类： 1XXXX
 class MenuResponse:
     @staticmethod
     def success(data, safe=True):
@@ -21,4 +22,22 @@ class MenuResponse:
     @staticmethod
     def other(data, safe=True):
         response_data = {"status": 10002, "data": data}
+        return JsonResponse(response_data, safe=safe)
+
+
+# 商品响应类： 2XXXX
+class GoodsResponse:
+    @staticmethod
+    def success(data, safe=True):
+        response_data = {"status": 20000, "data": data}
+        return JsonResponse(response_data, safe=safe)
+
+    @staticmethod
+    def failed(data, safe=True):
+        response_data = {"status": 20001, "data": data}
+        return JsonResponse(response_data, safe=safe)
+
+    @staticmethod
+    def other(data, safe=True):
+        response_data = {"status": 20002, "data": data}
         return JsonResponse(response_data, safe=safe)
