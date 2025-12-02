56df94b24e84dd2c429e0feee5febc3209d8f9cd
ba1p1ao <1732734010@qq.com>
2025-12-02 20:20:30 +0800

    添加: 购物车查找和删除

--- muxishop/apps/cart/views.py
+++ muxishop/apps/cart/views.py
@@ -12,7 +12,7 @@ class CartAPIView(APIView):
     # 修改或者添加购物车数据
     def post(self, request: HttpRequest):
         request_data = request.data
-        print("request_data", request_data)
+        # print("request_data", request_data)
         email = request_data.get("email")
         sku_id = request_data.get("sku_id")
         nums = request_data.get("nums")
@@ -27,6 +27,7 @@ class CartAPIView(APIView):
             cart.nums += nums
             # 保存数据
             cart.save()
+            return CartResponse.success("更新成功", safe=False)
         else:
             request_data["is_delete"] = 0 # 添加字段
             # 反序列化
@@ -35,10 +36,34 @@ class CartAPIView(APIView):
             new_cart_data.is_valid()
             # print(new_cart_data)
             cart = models.Cart.objects.create(**new_cart_data.data)
+            return CartResponse.success("添加成功", safe=False)
 
-        result = CartSerializer(instance=cart).data
+
+
+    # 查询购物车数据
+    def get(self, request: HttpRequest):
+        email = request.GET.get("email")
+
+        # 筛选合法数据
+        carts = models.Cart.objects.filter(email=email, is_delete=0)
+
+        # 查询结果为列表多个的时候使用 many=True 对列表进行序列化
+        result = CartSerializer(instance=carts, many=True).data
+
         return CartResponse.success(result, safe=False)
+    
+    def delete(self, request: HttpRequest):
+        request_data = request.data
+        email = request_data.get("email")
+        sku_id = request_data.get("sku_id")
 
+        # 查询未删除的合法数据
+        carts = models.Cart.objects.filter(email=email, sku_id=sku_id, is_delete=0).first()
+        if carts:
+            carts.is_delete = 1
+            carts.save()
 
-
+            return CartResponse.success("删除成功", safe=False) 
+        else:
+            return CartResponse.failed("数据不存在", safe=False)
 
